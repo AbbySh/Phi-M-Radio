1c9406a426405e55f57c1f3621a887175d80a2b0
Abby <abbys@ipac.caltech.edu>
2023-02-15 12:47:36 -0500

    Pushing before major changes to data cleaning.

---
+++ alg/StellarRadio_alg.py
@@ -0,0 +1,223 @@
+import numpy as np
+from scipy.optimize import minimize
+from astropy.timeseries import LombScargle
+import scipy.ndimage
+import lightkurve as lk
+import configparser
+import pickle
+
+class StellarRadioAlg:
+
+    def __init__(self):
+        parser = configparser.ConfigParser()
+        parser.read('stellar.cfg')
+        self.instrument = str(parser.get('STARQUALS','instrument'))
+        self.id = int(parser.get('STARQUALS','id'))
+        self.cadence = str(parser.get('STARQUALS','cadence'))
+        self.amp0 = float(parser.get('MATH','amp0'))
+        self.phase0 = float(parser.get('MATH','phase0'))
+        self.iters = int(parser.get('MATH','iters'))
+        self.period = float(parser.get('FIGS','period'))
+
+        if self.instrument == 'kepler':
+            self.ins_prefix = 'kic'
+        elif self.instrument == 'tess':
+            self.ins_prefix = 'tic'
+
+    def freq_finder(self,time,flux,qmin=0.,qmax=np.Inf):
+        """Finds frequency based off of LombScargle peaks.
+
+        Args:
+            time (np.ndarray): Time data.
+            flux (np.ndarray): Flux data.
+            qmin (float): Minimum for frequency choice.
+            qmax (float): Maximum for frequency choice.
+
+        Returns:
+            float: Frequency estimate that is improved upon in optimization step.
+            np.ndarray: Frequency (x-) component of LombScargle.
+            np.ndarray: Power (y-) component of LombScargle.
+        """            
+        q,y = LombScargle(time,flux).autopower()
+        f0_guess = q[np.argmax(y * (q > qmin) * (q < qmax))]
+        print("Frequency guess:",f0_guess)
+
+        return f0_guess,q,y
+
+    def get_lc_data(self):
+        """Gets light curve data using Lightkurve.
+
+        Returns:
+            np.ndarray: Time data.
+            np.ndarray: Flux data.
+            np.ndarray: Number of light curve sectors.
+        """
+        if type(self.id) != str:
+            self.id = str(self.id)
+
+        if self.instrument == 'tess':
+            try:
+                lc_files = lk.search_lightcurve('TIC'+self.id, cadence='short').download_all()
+                print('cadence type: short')
+            except:
+                lc_files = lk.search_lightcurve('TIC'+self.id, cadence='long').download_all()
+                print('cadence type: long')
+
+        elif self.instrument == 'kepler':
+            lc_files = lk.search_lightcurve('KIC'+self.id, cadence=self.cadence).download_all()
+            print('cadence type: {}'.format(self.cadence))
+
+        else:
+            raise TypeError('Only Kepler and TESS are supported instruments')
+            
+        time, flux, quarter = np.array([]), np.array([]), np.array([])
+        for q,lc in enumerate(lc_files):
+
+            time_table = lc['time']
+            this_time = []
+            for val in range(0,len(time_table)):
+                time_value = time_table[val].value
+                this_time.append(time_value)
+
+            this_flux = lc['pdcsap_flux']
+            good = np.isfinite(this_time)
+            
+            median_flux = np.nanmedian(this_flux)
+            this_flux = this_flux[good] / median_flux
+            # this_flerr = lc['pdcsap_flux_err'][good] / median_flux
+            this_q = np.zeros_like(this_time) + q
+            
+            bad = np.logical_not(np.isfinite(this_flux))
+            this_flux[bad] = 1.
+            # this_flerr[bad] = 1000.
+            time = np.concatenate((time,this_time))
+            flux = np.array(np.concatenate((flux,this_flux)))
+            # flerr = np.array(np.concatenate((flerr,this_flerr)))
+            quarter = np.concatenate((quarter, this_q))
+
+            ###mask hack###
+            t0 = 0
+            per = 58
+            for i in np.linspace(0,100):
+                event_time = t0 + (i*per)
+                mask = (time > (event_time - .5)) & (time < (event_time + .5))
+                time = time[~mask]
+                flux = flux[~mask]
+
+        quarter = np.round(quarter).astype(int)
+
+        flux=flux-1.
+
+        return time,flux,quarter
+
+    def eclipse_mask(self):
+        """
+        Masks out eclipses from light curve.
+        """
+
+
+    def mix(self,time,flux,f0):
+        """Carrier wave for flux. ? Check discription.
+
+        Args:
+            time (np.ndarray): Time data.
+            flux (np.ndarray): Flux data.
+            f0 (float): Frequency guess.
+
+        Returns:
+            np.ndarray: Real components of mixer multiplied by flux.
+            np.ndarray: Imaginary components of mixer multiplied by flux.
+            np.ndarray: Gaussian-filtered remf.
+            np.ndarray: Gaussian-filtered immf.
+        """
+        mixer = self.amp0 * np.exp(1j * (2 * np.pi * f0 * time - self.phase0))
+        mf = mixer * flux
+        remf = mf.real
+        immf = mf.imag
+        sremf = scipy.ndimage.filters.gaussian_filter1d(remf, 50)
+        simmf = scipy.ndimage.filters.gaussian_filter1d(immf, 50)
+        print('mix:',self.amp0,f0,self.phase0,np.median(flux),np.min(flux),np.mean(flux),np.mean(mf))
+        return sremf,simmf
+
+    def objective(self,f,time,flux,amp,phase):
+        """Gets variance of imaginary component of the mixer multiplied by flux.
+
+        Args:
+            f (float): Frequency
+            time (np.ndarray): Time data.
+            flux (np.ndarray): Flux data.
+            amp (float): Amplitude value.
+            phase (float): Phase value.
+
+        Returns:
+            np.ndarray: Variance of imaginary component of mixer multiplied by float.
+        """
+        mixer = amp * np.exp(1j * (2 * np.pi * f * time - phase))
+        mf = mixer * flux
+        return np.var(mf.imag)
+
+    def optimization(self,time,flux,f0):
+        """Frequency optimization iterations.
+
+        Args:
+            time (np.ndarray): Time data.
+            flux (np.ndarray): Flux data.
+            f0 (float): Frequency guess.
+
+        Returns:
+            np.ndarray: Optimized Gaussian-filtered immf.
+        """
+        for i in range(self.iters):
+            print("Iteration", i)
+            
+            sremf,simmf = self.mix(time,flux,f0)
+
+            new_amp = self.amp0 / np.sqrt(np.mean(sremf**2 + simmf**2))
+            print(np.mean(sremf**2 + simmf**2),np.mean(sremf**2),np.mean(simmf**2))
+            print("Amps:", self.amp0, new_amp)
+            self.amp0 = new_amp
+            
+            sremf,simmf = self.mix(time,flux,f0)
+            new_phase = self.phase0 + np.arctan2(np.mean(simmf), np.mean(sremf))
+            print("Phases:", self.phase0, new_phase)
+            self.phase0 = new_phase
+        
+            sremf, simmf = self.mix(time,flux,f0)
+
+            output=minimize(self.objective,f0,args=(time,flux,self.amp0,self.phase0),method="Nelder-Mead")
+            print("Frequencies:", f0, np.mean((output.final_simplex[0][0],output.final_simplex[0][1])))
+            f0=np.mean((output.final_simplex[0][0],output.final_simplex[0][1]))
+            
+        return simmf
+
+    def run_all_steps(self, injection_flux=None,qmin=0,qmax=np.Inf): # flux hack! Instead this should take injection parameters.
+        """Runs algorithm steps in order, which ends in plotting a LombScargle
+        periodogram of time vs simmf.
+
+        Args:
+            injection_flux (np.ndarray): Flux to be injected into stellar object flux. Used for testing. Defaults to None.
+            qmin (float): Minimum boundary for frequency guess. Defaults to 0.
+            qmax (float): Maximum boundary for frequency guess. Defaults to infinity.
+
+        Returns:
+            np.ndarray: Time data.
+            np.ndarray: Flux data.
+            np.ndarray: Number of light curve sectors.
+            np.ndarray: Optimized Gaussian-filtered immf.
+        """
+        time,flux,quarter = self.get_lc_data()
+
+        if injection_flux is not None:
+            flux = injection_flux
+
+        f0,q,y = self.freq_finder(time,flux,qmin=qmin,qmax=qmax)
+
+        simmf = self.optimization(time,flux,f0)
+
+        plot_vals = {'flux':flux,'time':time,'quarter':quarter,'f0':f0,'q':q,'y':y,'simmf':simmf}
+
+        with open('./pickle_files/pickle_{}_{}.pkl'.format(self.ins_prefix,self.id),'wb') as file:
+            pickle.dump(plot_vals,file)
+            file.close()
+        print ('done')
+        return time,flux,quarter,simmf
---
+++ alg/StellarRadio_plotting.py
@@ -0,0 +1,159 @@
+import matplotlib.pyplot as plt
+import configparser
+from astropy.timeseries import LombScargle
+import pickle
+import numpy as np
+from scipy.stats import binned_statistic as bs
+from alg.StellarRadio_alg import StellarRadioAlg
+
+class StellarRadioAlg_Plotting:
+
+    def __init__(self):
+        parser = configparser.ConfigParser()
+        parser.read('stellar.cfg')
+        self.freq_toggle = bool(parser.get('FIGS','freq_plot_toggle'))
+        self.lombscargle_toggle = bool(parser.get('FIGS','lombscargle_toggle'))
+        self.lightcurve_toggle = bool(parser.get('FIGS','lightcurve_toggle'))
+        self.instrument = str(parser.get('STARQUALS','instrument'))
+        self.id = int(parser.get('STARQUALS','id'))
+        self.period = float(parser.get('FIGS','period'))
+
+        if self.instrument == 'kepler':
+            self.ins_prefix = 'kic'
+        elif self.instrument == 'tess':
+            self.ins_prefix = 'tic'
+
+    def load_values(self):
+        """
+        Loads values from pickle file, or, performs stellar radio algorithm if no pickle file for stellar object exists. 
+
+        Returns:
+            np.ndarray: Time data.
+            np.ndarray: Flux data.
+            np.ndarray: Number of light curve sectors.
+            float: Guess for pulsation frequency.
+            np.ndarray: Frequency (x-) component of LombScargle.
+            np.ndarray: Power (y-) component of LombScargle.
+            np.ndarray: Optimized Gaussian-filtered immf.
+        """
+        try:
+            with open('./pickle_files/pickle_{}_{}.pkl'.format(self.ins_prefix,self.id),'rb') as file:
+                print('pickle file exists for object')
+                vals = pickle.load(file)
+                time = vals['time']
+                flux = vals['flux']
+                quarter = vals['quarter']
+                f0 = vals['f0']
+                q = vals['q']
+                y = vals['y']
+                simmf = vals['simmf']
+            
+        except:
+            print('pickle file does not exist: running necessary algorithmic steps')
+            alg_init = StellarRadioAlg()
+            alg_init.run_all_steps()
+            with open('./pickle_files/pickle_{}_{}.pkl'.format(self.ins_prefix,self.id),'rb') as file:
+                vals = pickle.load(file)
+                time = vals['time']
+                flux = vals['flux']
+                quarter = vals['quarter']
+                f0 = vals['f0']
+                q = vals['q']
+                y = vals['y']
+                simmf = vals['simmf']
+
+        return time,flux,quarter,f0,q,y,simmf
+
+    def lightcurve_plot(self,time,flux,quarter,f0):
+        """
+        Plots lightcurve.
+
+        Args:
+            time (np.ndarray): Time data
+            flux (np.ndarray): Flux data
+            quarter (np.ndarray): Number of light curve sectors.
+            f0 (float): Guess for pulsation frequency.
+        """
+        plt.figure()
+        plt.title('Lightcurve: {}{} PDCSAP Flux'.format(self.ins_prefix,self.id))
+        plt.scatter(time,flux,c=quarter,marker='.',s=5,alpha=.7)
+        plt.ylabel('Flux (arbitrary units)')
+        plt.xlabel('Barycentric Julian Date Plus Offset (d)')
+        plt.savefig('./lightcurves/stellar_radio_lightcurve_{}_{}.pdf'.format(self.ins_prefix,str(self.id)))
+        plt.show()
+        plt.close()
+
+        t0 = 1/f0
+        folded_time = time % t0
+        means,bin_edges,_ = bs(folded_time,flux,bins=100)
+        bin_centers = .5*(bin_edges[1:]+bin_edges[:-1])
+
+        plt.figure()
+        plt.title('Lightcurve: {}{} Folded Flux'.format(self.ins_prefix,self.id))
+        plt.scatter(folded_time,flux,c=quarter,marker='.',s=5,alpha=.03)
+        plt.scatter(folded_time+t0,flux,c=quarter,marker='.',s=5,alpha=.03)
+        plt.plot(bin_centers,means,'ko')
+        plt.ylabel('Flux (arbitrary units)')
+        plt.xlabel('Modded Time (d)')
+        plt.savefig('./lightcurves/stellar_radio_lightcurve_{}_{}_folded.pdf'.format(self.ins_prefix,str(self.id)))
+        plt.show()
+        plt.close()
+
+    def freq_guess_plot(self,time,q,y,f0):
+        """
+        Plots frequency guess plot (LombScargle).
+
+        Args:
+            time (np.ndarray): Time data.
+            q (np.ndarray): Frequency (x-) component of LombScargle.
+            y (np.ndarray): Power (y-) component of LombScargle.
+            f0 (float): Guess for pulsation frequency.
+        """
+        sampling_freq = 1./np.nanmedian(time[1:]-time[:-1])
+        plt.figure()
+        plt.title('LombScargle Periodogram: {}{} Frequency Guess'.format(self.ins_prefix,self.id))
+        plt.plot(q,y,c='purple',alpha=.5)
+        plt.axvline(sampling_freq,c='black',alpha=.25)
+        plt.axvline(f0,alpha=.5,color='green')
+        plt.savefig('./frequency_guess_plots/stellar_radio_frequency_guess_{}_{}.pdf'.format(self.ins_prefix,str(self.id)))
+        plt.xlabel('Frequency (1/d)')
+        plt.ylabel('Measurement Values (arbitrary units)')
+        plt.xlim(0,20)
+        plt.show()
+        plt.close()
+
+    def final_lombscargle_plot(self,time,simmf):
+        """Sets up plot for final LombScargle periodogram.
+
+        Args:
+            time (np.ndarray): Time data
+            simmf (np.ndarray): Optimized Gaussian-filtered immf
+        """
+        q,y = LombScargle(time,simmf).autopower()
+        plt.plot(1./q,y,c='black',alpha=.75)
+        plt.title('LombScargle Periodogram: {}{} Period vs Gaussian-filtered Im(mixer * flux)'.format(self.ins_prefix,self.id))
+        plt.xlabel("Period (days)")
+        plt.ylabel('"Simmf" (arbitrary units)')
+        plt.xlim(1,100)
+        plt.grid()
+        plt.loglog()
+        plt.axvline(self.period,c='mediumslateblue',alpha=.6,label="expected companion orbital period (literature): {}".format(self.period),zorder=-10)
+        plt.legend()
+        plt.savefig('./LS_periodograms/stellar_radio_plot_{}_{}.pdf'.format(self.ins_prefix,str(self.id)))
+        plt.show()
+        plt.close()
+
+    def do_plots(self):
+        """
+        Plots the plots.
+        """
+        time,flux,quarter,f0,q,y,simmf = self.load_values()
+
+        if self.lightcurve_toggle == True:
+            self.lightcurve_plot(time,flux,quarter,f0)
+
+        if self.freq_toggle == True:
+            self.freq_guess_plot(time,q,y,f0)
+
+        if self.lombscargle_toggle == True:
+            self.final_lombscargle_plot(time,simmf)
